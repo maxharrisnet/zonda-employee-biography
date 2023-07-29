edf954774d704d3a6fa18ca1430baa50d923329a
maxharrisnet <hello@maxharris.codes>
2023-07-28 17:17:15 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# zonda-employee-biography
---
+++ zonda-employee-biography.php
@@ -0,0 +1,14 @@
+<?php
+/*
+* Plugin Name: Zonda Employee Bios
+* Description: This plugin is for a coding assessment for the Senior Web Developer (WordPress CMS) role at Zonda. The function of the plugin is to create a shortcode that displays biographical information of employees.
+* Version: 1.0
+* Author: Max Harris
+* Author URI: https://github.com/maxharrisnet
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
