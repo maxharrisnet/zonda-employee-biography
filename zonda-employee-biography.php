65a1e5db106896081741d558e4fb775d7e20b6ab
maxharrisnet <hello@maxharris.codes>
2023-08-01 17:51:41 -0700

    registering shortcode, setting up query and output

--- zonda-employee-biography.php
+++ zonda-employee-biography.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Include ACF locally
+// TODO: Include ACF locally https://www.advancedcustomfields.com/resources/including-acf-within-a-plugin-or-theme/
 
 function zonda_register_post_type() {
   $labels = array(
@@ -30,7 +30,7 @@ function zonda_register_post_type() {
 
   $args = array(
     'labels' =>  $labels,
-    'description' => 'A post type for collecting and displaying employee information.',
+    'description' => __('A post type for collecting and displaying employee information.', 'zonda'),
     'public' => true,
     'supports' => array('custom_fields')
   );
@@ -55,25 +55,24 @@ function zonda_register_taxonomy() {
 
   $args = array(
     'labels' =>  $labels,
-    'description' => 'A taxonomy that represents the various divisions in the company in which employees are organized.',
-    'public' => true
+    'description' => __('A taxonomy that represents the various divisions in the company in which employees are organized.', 'zonda'),
+    'public' => true,
+    'meta_box_cb' => false,
+
   );
 
-  // Add image meta
-
-  register_taxonomy( 'division', 'zonda_employee', $args );
+  register_taxonomy( 'zonda_division', 'zonda_employee', $args );
 }
 add_action( 'init', 'zonda_register_taxonomy' );
 
-function zonda_register_post_meta_fields() {
-// Register ACF group/fields
+function zonda_register_meta_fields() {
   acf_add_local_field_group(array (
     'key' => 'employee_information',
-    'title' => 'Employee\'s Biographic Information',
+    'title' => __('Employee\'s Biographic Information', 'zonda'),
     'fields' => array(
       array(
         'key' => 'first_name',
-        'label' => 'First Name',
+        'label' => __('First Name', 'zonda'),
         'name' => 'first_name',
         'type' => 'text',
         'maxlength' => '100',
@@ -84,7 +83,7 @@ function zonda_register_post_meta_fields() {
       ),
       array(
         'key' => 'last_name',
-        'label' => 'Last Name',
+        'label' => __('Last Name', 'zonda'),
         'name' => 'last_name',
         'type' => 'text',
         'maxlength' => '100',
@@ -95,10 +94,10 @@ function zonda_register_post_meta_fields() {
       ),
       array(
         'key' => 'bio_image',
-        'label' => 'Image',
+        'label' => __('Image', 'zonda'),
         'name' => 'bio_image',
         'type' => 'image',
-        'instructions' => 'Height/Width 200px >< 1200px File size < 1MB',
+        'instructions' => __('Height/Width 200px >< 1200px File size < 1MB', 'zonda'),
         'instruction_placement' => 'field',
         'required' => false,
         'return_format' => 'url',
@@ -107,13 +106,14 @@ function zonda_register_post_meta_fields() {
         'max_width' => 1200,
         'max_height' => 1200,
         'max_size' => '1MB',
+        // 'mime_types' => 'image/jpeg, image/pjpeg, image/png, image/webp', // TODO: Add webp to WordPress MIME types https://www.isitwp.com/modify-allowed-upload-mime-types/
         'wrapper' => array(
           'width' => '20%'
         )
       ),
       array(
         'key' => 'position_title',
-        'label' => 'Position',
+        'label' => __('Position', 'zonda'),
         'name' => 'position_title',
         'type' => 'text',
         'maxlength' => '100',
@@ -124,10 +124,10 @@ function zonda_register_post_meta_fields() {
       ),
       array(
         'key' => 'division_title',
-        'label' => 'Division',
+        'label' => __('Division', 'zonda'),
         'name' => 'division_title',
         'type' => 'taxonomy',
-        'taxonomy' => 'division',
+        'taxonomy' => 'zonda_division',
         'field_type' => 'multi_select',
         'return_format' => 'object',
         'add_term' => 'false',
@@ -138,10 +138,10 @@ function zonda_register_post_meta_fields() {
       ),
       array(
         'key' => 'start_date',
-        'label' => 'Start Date',
+        'label' => __('Start Date', 'zonda'),
         'name' => 'start_date',
         'type' => 'text',
-        'instructions' => 'Please enter the date for the first day of employment for the employee.',
+        'instructions' => __('Please enter the date for the first day of employment for the employee.', 'zonda'),
         'instruction_placement' => 'field',
         'required' => true,
         'wrapper' => array(
@@ -150,7 +150,7 @@ function zonda_register_post_meta_fields() {
       ),
       array(
         'key' => 'bio',
-        'label' => 'Bio',
+        'label' => __('Bio', 'zonda'),
         'name' => 'bio',
         'type' => 'textarea',
         'required' => true,
@@ -171,37 +171,121 @@ function zonda_register_post_meta_fields() {
       )
     )
   ));
+  /* TODO: change columns in admin screen -> first name, last name, division, position 
+  * https://www.smashingmagazine.com/2017/12/customizing-admin-columns-wordpress/
+  * https://developer.wordpress.org/reference/hooks/manage_post_type_posts_columns/
+  * https://pluginrepublic.com/add-acf-fields-to-admin-columns/
+  */
 
-  // Validation and santization
+  // TODO: use site title for comapny name in admin screen
+
+  acf_add_local_field_group(array (
+    'key' => 'division_information',
+    'title' => __('Information regarding company divisions', 'zonda'),
+    'fields' => array(
+      array(
+        'key' => 'division_image',
+        'label' => __('Image', 'zonda'),
+        'name' => 'division_image',
+        'type' => 'image',
+        'instructions' => __('Height/Width 200px >< 600px File size < 1MB', 'zonda'),
+        'instruction_placement' => 'field',
+        'required' => false,
+        'return_format' => 'url',
+        'min_width' => 200,
+        'min_height' => 200,
+        'max_width' => 600,
+        'max_height' => 600,
+        'max_size' => '1MB',
+        'wrapper' => array(
+          'width' => '100%'
+        )
+      ),
+    ),
+    'position' => 'normal',
+    'location' => array ( 
+      array (
+        array (
+          'param' => 'taxonomy',
+          'operator' => '==',
+          'value' => 'zonda_division'
+        )
+      )
+    )
+  ));
+
+  // TODO: limit division entries to Admin users only
 }
-add_action( 'acf/init', 'zonda_register_post_meta_fields' );
+add_action( 'acf/init', 'zonda_register_meta_fields' );
 
-// function zonda_register_shortcode() {
+function zonda_register_shortcode() {
 // Register shortcode
-// Markup for container
-// Markup for employee cards
-// }
+
+  function zonda_employee_biography_template( $atts ) {
+    // Normalizing key case
+    $atts = array_change_key_case( (array) $atts, CASE_LOWER );
+    // Setting the acceptable attribute pairs
+    extract(shortcode_atts(array (
+      ids => [],
+      divisions => []
+    ), $atts));
+    
+    $args = array (
+      'post_type' => 'zonda_employee',
+      'numberposts' => -1,
+      'include' => $atts['ids'],
+      'suppress_filters' => false
+    );
+
+    // TODO: Add a `tax_query` to args for searching by department 
+
+    $employees = get_posts($args);
+    $output = '<section class="zonda-employees-container">';
+    $output .= 'Bingo! ' . $ids;
+
+    // Loop through employees
+    if( $employees ) {
+      foreach( $employees as $e ) {
+
+        setup_postdata( $e );
+
+        // Cards
+        $output .= '<div>';
+        $output .= '<h3>#' . the_field('first_name') . ' ' . the_field('last_name') . '</h3>';
+        $output .= '</div>';
+      }
+    }
+    $output .= '</section>';
+    
+    wp_reset_postdata();
+
+    return $output;
+  }
+
+  add_shortcode( 'zonda_employee_biography', 'zonda_employee_biography_template' );
+}
+add_action( 'init', 'zonda_register_shortcode' );
 
 function zonda_activate() {
   zonda_register_post_type();
   zonda_register_taxonomy();
-  zonda_register_post_meta_fields();
-  // zonda_register_shortcode();
+  zonda_register_meta_fields();
+  zonda_register_shortcode();
   flush_rewrite_rules();
 }
+register_activation_hook( __FILE__, 'zonda_activate');
 
 function zonda_deactivate() {
   unregister_post_type( 'zonda_employee' );
   flush_rewrite_rules();
 }
-
-register_activation_hook( __FILE__, 'zonda_activate');
 register_deactivation_hook( __FILE__, 'zonda_deactivate');
 
+// Remove shortcode on deactivation?
 
-// Output template (escape)
-
-
-// Remove shortcode 
+// [] Field validation
+// [] Sanitization
+// [] Escaping
+// [] Localization
 
 ?>
